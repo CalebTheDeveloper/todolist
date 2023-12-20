4e552fca35c67da6583f9cce576a71f47c14ef60
CalebTheDeveloper <150725721+CalebTheDeveloper@users.noreply.github.com>
2023-12-19 19:32:34 -0700

    This is the modified copy of the Altcademy repo.  The Controllers file has been updated with the required code to make the API function.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,35 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+    render json: @tasks
+  end
+
+  def create
+    task_params = params.require(:task).permit(:content)
+    @task = Task.new(task_params)
+
+    if @task.save
+      render json: @task, status: :created
+    else
+      render json: @task.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    head :no_content
+  end
+  
+  def mark_complete
+    task = Task.find(params[:id])
+    task.update(completed: true)
+    
+  end
+  
+  def mark_active
+    task = Task.find(params[:id])
+    task.update(completed: false)
+    
+  end
 end
